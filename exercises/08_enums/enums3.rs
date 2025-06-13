0e65d44707cb248b0c78f2b8d474c9982c2b4d7b
Ethan <kingscout1011@gmail.com>
2025-06-13 13:58:50 -0500

    rut slings

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -5,6 +5,11 @@ struct Point {
 
 enum Message {
     // TODO: Implement the message variant types based on their usage below.
+    Move(Point),
+    Resize { width: u64, height: u64 },
+    Quit,
+    Echo(String),
+    ChangeColor(u8, u8, u8),
 }
 
 struct State {
@@ -42,6 +47,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Move(point) => self.move_position(point),
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Quit => self.quit(),
+            Message::Echo(message) => self.echo(message),
+            Message::ChangeColor(red, green, blue) => self.change_color(red, green, blue),
+        }
     }
 }
 
